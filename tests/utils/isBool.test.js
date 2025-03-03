5156f3e6db36943b4c3f00460ecf406d1a970a26
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2025-03-03 20:33:10 +0800

    Added 1 test case

--- tests/utils/isBool.test.js
+++ tests/utils/isBool.test.js
@@ -1,38 +1,30 @@
 import { describe, expect, it, vi } from 'vitest'
-
-const isBool = (bool) => {
-    if (bool) {
-        if (bool === 'false') {
-            return false
-        }
-        return true
-    } else {
-        return false
-    }
-}
+import { toBool } from '../../utils'
 
 describe('isBool helper function', () => {
     it('returns false if param is undefined', () => {
-        expect(isBool(undefined)).toBe(false)
+        expect(toBool(undefined)).toBe(false)
     })
 
     it('returns false if param is empty string', () => {
-        expect(isBool('')).toBe(false)
+        expect(toBool('')).toBe(false)
     })
 
     it('returns false if param is a string: false', () => {
-        expect(isBool('false')).toBe(false)
+        expect(toBool('false')).toBe(false)
     })
 
     it('returns true if param is a truthy', () => {
-        expect(isBool('true')).toBe(true)
-        expect(isBool(1)).toBe(true)
+        expect(toBool('true')).toBe(true)
+        expect(toBool(1)).toBe(true)
     })
     it('returns true if param is actual boolean true', () => {
-        expect(isBool(true)).toBe(true)
+        expect(toBool(true)).toBe(true)
     })
     it('returns false if param is actual boolean false', () => {
-        expect(isBool(false)).toBe(false)
-
+        expect(toBool(false)).toBe(false)
+    })
+    it('returns false if param is null', () => {
+        expect(toBool(null)).toBe(false)
     })
 })
