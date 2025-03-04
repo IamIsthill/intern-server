bd8fb86e6b5cc40fbf11abf2665a413011cbf5f3
Charles Bercasio <159409061+IamIsthill@users.noreply.github.com>
2025-03-04 09:43:16 +0800

    Revert "Cors"

--- database/connectDb.js
+++ database/connectDb.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose')
 const { DATABASE_URI } = require('../config')
-
 const connectDb = async () => {
     try {
         await mongoose.connect(DATABASE_URI)
     } catch (e) {
-        console.log('Database error: ', e.message)
+        console.log('Database error: ', e)
     }
 }
 
