7b243ae261cb144740aa9c1edcfbc88f72916d4c
Gabs <meridorgabrield@gmail.com>
2025-03-05 13:33:31 +0800

    added reports and tasks model

---
+++ models/Tasks.js
@@ -0,0 +1,20 @@
+import mongoose from mongoose;
+
+const tasksSchema = new mongoose.Schema({
+    supervisor: { type: mongoose.Schema.Types.ObjectId, ref: "Supervisor" },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    status: { type: String,
+         required: true,
+         enum: ["pending", "in-progress", "completed", "backlogs"],
+         default: "pending",},
+    deadline: { type: Date},
+    assignedInterns: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Intern",
+        },
+    ],
+});
+
+export const Tasks = mongoose.model("Tasks", tasksSchema);
