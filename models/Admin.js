86f954f355bd250db207f14a1d5ec65493b19755
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2025-03-05 13:03:41 +0800

    Created admin schema

---
+++ models/Admin.js
@@ -0,0 +1,35 @@
+import { required } from "joi";
+import mongoose from "mongoose";
+
+const adminSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        minLenght: 8,
+        type: String,
+    },
+    accountType: {
+        type: String,
+        required: true,
+        default: 'admin'
+    }
+})
+
+export let Admin
+
+if (mongoose.models.Admin) {
+    Admin = mongoose.model('Admin')
+} else {
+    Admin = mongoose.model('Admin', adminSchema)
+}
