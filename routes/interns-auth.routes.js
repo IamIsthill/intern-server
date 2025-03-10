fae78f808ac85f18d0ac0c7cc79ea93283560ba0
Gabs <meridorgabrield@gmail.com>
2025-03-10 08:17:08 +0800

    renamed auth to materalized

--- routes/interns-auth.routes.js
+++ routes/interns-auth.routes.js
@@ -5,10 +5,9 @@ import {
   checkEmailAvailability,
   loginInternController,
   checkPhoneAvailability,
-} from "../controllers/interns-auth-controller.js";
-import { adminLoginController } from '../controllers/admin.controller.js'
+} from "../controllers/interns-auth.controller.js";
+import { adminLoginController } from "../controllers/admin.controller.js";
 import { loginSupervisorController } from "../controllers/supervisor.controller.js";
-
 
 router.post("/register", registerInternController);
 router.get("/check-email", checkEmailAvailability);
@@ -17,5 +16,3 @@ router.get("/check-phone", checkPhoneAvailability);
 router.post("/login/intern", loginInternController);
 router.post("/login/admin", adminLoginController);
 router.post("/login/supervisor", loginSupervisorController);
-
-
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import "dotenv/config";
-import mongoose from "mongoose";
 import express from "express";
 import compression from "compression";
 import { connectDb, startApp, onDbError } from "./database/index.js";
@@ -11,29 +10,29 @@ import { adminRouter } from "./routes/admin.routes.js";
 import { supervisorRouter } from "./routes/supervisor.routes.js";
 import { departmentRouter } from "./routes/department.routes.js";
 import { internRouter } from "./routes/interns.routes.js";
-import { router as internAuthRouter } from "./routes/interns-auth-routes.js";
-
-
+import { router as internAuthRouter } from "./routes/interns-auth.routes.js";
 
 export const app = express();
 const port = 3000;
 
 connectDb();
 
+console.log(
+  "Intern Auth Routes Loaded:",
+  internAuthRouter.stack.map((layer) => layer.route?.path)
+);
+
 app.use(compression());
 app.use(express.json());
 app.use(Cors());
 app.use("/auth", internAuthRouter);
-app.use(authenticateJWT)
-app.use('/interns', internRouter)
+app.use(authenticateJWT);
+app.use("/interns", internRouter);
 app.use("/admin", adminRouter);
-app.use('/tasks', taskRouter)
-app.use('/supervisors', supervisorRouter)
-app.use('/departments', departmentRouter)
+app.use("/tasks", taskRouter);
+app.use("/supervisors", supervisorRouter);
+app.use("/departments", departmentRouter);
 app.use(errorHandler);
 
-
-startApp(app, port)
-onDbError()
-
-
+startApp(app, port);
+onDbError();
