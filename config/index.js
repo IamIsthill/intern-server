b1ceab10837e8bc4b17fdfb80f8cabef7cac74c3
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2025-03-20 20:50:22 +0800

    created send email service

--- config/index.js
+++ config/index.js
@@ -3,5 +3,7 @@ import { toBool } from '../utils/index.js'
 
 export const DEVELOPMENT = toBool(process.env.DEVELOPMENT)
 export const JWT_SECRET = process.env.JWT_SECRET
+export const EMAIL_PASS = process.env.EMAIL_PASS
+export const EMAIL = process.env.EMAIL
 export * from './database.config.js'
 
