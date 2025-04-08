affc6247cdc2c5974b50ceae7b9dcfd3f06e8a33
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2025-04-08 17:18:49 +0800

    feat: add scheduled job to send old server logs via email

--- services/mail.js
+++ services/mail.js
@@ -36,3 +36,28 @@ export const sendEmail = async (toEmail, subject, htmlContent) => {
     });
   });
 };
+
+
+
+export const sendLogs = async (subject, text, logs) => {
+  const mailOptions = {
+    from: `A2K Group <${EMAIL}>`,
+    to: EMAIL,
+    subject: subject,
+    text: text,
+    attachments: logs
+  };
+
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        logger.error(`Unable to send email: ${error.message}`)
+        reject(new Error(`Unable to send email: ${error.message}`));
+      } else {
+        logger.info("Email sent: ", info.response)
+        console.log("Email sent: ", info.response);
+        resolve(info.response);
+      }
+    });
+  });
+};
